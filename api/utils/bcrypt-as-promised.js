8b467861839cd2d179f3733d5998c6ba5db152d4
C.J. Winslow <whoaa512@gmail.com>
2015-11-22 13:14:07 -0800

    refactor login to use the new crypto stuff

--- api/utils/bcrypt-as-promised.js
+++ api/utils/bcrypt-as-promised.js
@@ -1,4 +1,14 @@
+import ApiError from 'utils/ApiError'
 import bcrypt_ from 'bcrypt'
+import logger from 'utils/logger'
 import Promise from 'bluebird'
 
 export const bcrypt = Promise.promisifyAll(bcrypt_)
+
+export function cryptoCatch (userId) {
+  return (err) => {
+    let errStr = `Bcrypt error. User id: ${userId}`
+    logger.error(err, errStr)
+    throw new ApiError(errStr)
+  }
+}
