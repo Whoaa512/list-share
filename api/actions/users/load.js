f99d0816ef4d9c794e85ffd2de7589a80134dbf6
C.J. Winslow <whoaa512@gmail.com>
2015-11-22 14:20:26 -0800

    add users load route

---
+++ api/actions/users/load.js
@@ -0,0 +1,38 @@
+import ApiError from 'utils/ApiError'
+import { usersCollection } from 'utils/db-collections'
+
+export function getUser (email, userId) {
+  let user = null
+
+  if (userId != null) {
+    user = usersCollection.find({
+      id: userId
+    })
+  } else if (email != null) {
+    user = usersCollection.find({
+      email
+    })
+  }
+
+  return user
+}
+
+export default function load (req) {
+  return new Promise((resolve, reject) => {
+    const {
+      email,
+      userId
+    } = req.body
+
+    if (email == null || userId == null) {
+      return reject(new ApiError('Missing email or user id'))
+    }
+
+    const user = getUser(email, userId)
+    if (user == null) {
+      return new ApiError('No user found for the given parameters')
+    }
+
+    resolve(user)
+  })
+}
