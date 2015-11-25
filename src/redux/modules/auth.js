2ed87f327934ddbca57bf4b7055f1e75b88c6a10
C.J. Winslow <whoaa512@gmail.com>
2015-11-25 11:47:36 -0800

    update login action creator to accept email and password

--- src/redux/modules/auth.js
+++ src/redux/modules/auth.js
@@ -84,12 +84,13 @@ export function load () {
   }
 }
 
-export function login (name) {
+export function login (email, password) {
   return {
     types: [LOGIN, LOGIN_SUCCESS, LOGIN_FAIL],
     promise: (client) => client.post('/login', {
       data: {
-        name: name
+        email,
+        password
       }
     })
   }
