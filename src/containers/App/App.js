686a60ab082583608c6ab6a80b171216ded88baa
C.J. Winslow <whoaa512@gmail.com>
2015-11-25 12:53:40 -0800

    change where we redirect on login and logout

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -40,10 +40,10 @@ export default class App extends Component {
   componentWillReceiveProps (nextProps) {
     if (!this.props.user && nextProps.user) {
       // login
-      this.props.pushState(null, '/loginSuccess')
+      this.props.pushState(null, '/')
     } else if (this.props.user && !nextProps.user) {
       // logout
-      this.props.pushState(null, '/')
+      this.props.pushState(null, '/login')
     }
   }
 
