c3b508e2a17cc717f3df4faee72406bc93fc36ce
C.J. Winslow <whoaa512@gmail.com>
2015-11-28 20:29:26 -0800

    render the actual lists

--- src/containers/Lists/Lists.js
+++ src/containers/Lists/Lists.js
@@ -1,23 +1,46 @@
 import config from 'config'
-import React, { Component } from 'react'
+import get from 'lodash.get'
+import React, { Component, PropTypes } from 'react'
 import DocumentMeta from 'react-document-meta'
+import { connect } from 'react-redux'
+import { getLists } from 'redux/modules/lists'
+import { getUsers } from 'redux/modules/users'
 import { ListRow } from 'components'
 
+@connect(mapStateToProps)
 export default class Lists extends Component {
+  static get propTypes () {
+    return {
+      lists: PropTypes.array.isRequired
+    }
+  }
 
   render () {
+    const { lists } = this.props
     return (
       <div className='container'>
         <h1>Lists</h1>
         <DocumentMeta title={`${config.app.title}: Lists`}/>
         <ul>
-          <ListRow
-              avatarImg='https://avatars1.githubusercontent.com/u/170270?v=3&s=60'
-              link='/list/:listId'
-              title="Alice's Christmas List"
-          />
+          {lists.map(list =>
+            <ListRow {...list} />
+          )}
         </ul>
       </div>
     )
+  }
+}
+
+function mapStateToProps (state) {
+  const users = getUsers(state)
+  let lists = getLists(state)
+  lists = Object.keys(lists).map(x => {
+    const list = lists[x]
+    list.avatarImg = get(users, list.creator)
+    list.link = `/list/${list.id}`
+    return list
+  })
+  return {
+    lists
   }
 }
