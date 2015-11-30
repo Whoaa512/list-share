c1a47cbafe251ebda444bbe220093ceae3e87597
C.J. Winslow <whoaa512@gmail.com>
2015-11-29 19:55:38 -0800

    add default for 0 lists on home page

--- src/containers/Lists/Lists.js
+++ src/containers/Lists/Lists.js
@@ -3,6 +3,8 @@ import config from 'config'
 import get from 'lodash.get'
 import React, { Component, PropTypes } from 'react'
 import DocumentMeta from 'react-document-meta'
+import { Link } from 'react-router'
+import { Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { getLists } from 'redux/modules/lists'
 import { getUsers } from 'redux/modules/users'
@@ -22,6 +24,14 @@ export default class Lists extends Component {
       <div className='container'>
         <h1>Lists</h1>
         <DocumentMeta title={`${config.app.title}: Lists`}/>
+        {lists.length <= 0 &&
+        <div>
+          <h4>No lists yet. Let's create the first!</h4>
+          <Link to='/create-list'>
+            <Button>Create New List</Button>
+          </Link>
+        </div>
+        }
         <ul>
           {lists.map(list =>
             <ListRow {...list} />
