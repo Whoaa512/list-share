34952476308911e98d39655d2a0a3a1ac1aad6a9
C.J. Winslow <whoaa512@gmail.com>
2015-11-30 10:21:14 -0800

    only add ga for prod

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -52,7 +52,9 @@ export default class Html extends Component {
         <body>
           <div id='content' dangerouslySetInnerHTML={{__html: content}}/>
           <script dangerouslySetInnerHTML={{__html: `window.__data=${serialize(store.getState())};`}} charSet='UTF-8'/>
+          {!__DEVELOPMENT__ &&
           <script dangerouslySetInnerHTML={{ __html: googleAnalyticsCode }} charSet='UTF-8'/>
+          }
           <script src={assets.javascript.main} charSet='UTF-8'/>
         </body>
       </html>
