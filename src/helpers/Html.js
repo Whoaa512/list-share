8caf7510afc5b62b4f70cc1f54855d1a089eb4d1
C.J. Winslow <whoaa512@gmail.com>
2015-11-29 23:57:46 -0800

    add analytics

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -22,6 +22,14 @@ export default class Html extends Component {
   render () {
     const {assets, component, store} = this.props
     const content = component ? ReactDOM.renderToString(component) : ''
+    const googleAnalyticsCode = `<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-70843519-1', 'auto');
+      ga('send', 'pageview');</script>`
 
     return (
       <html lang='en-us'>
@@ -45,6 +53,7 @@ export default class Html extends Component {
         <body>
           <div id='content' dangerouslySetInnerHTML={{__html: content}}/>
           <script dangerouslySetInnerHTML={{__html: `window.__data=${serialize(store.getState())};`}} charSet='UTF-8'/>
+          <script dangerouslySetInnerHTML={{ __html: googleAnalyticsCode }} charSet='UTF-8'/>
           <script src={assets.javascript.main} charSet='UTF-8'/>
         </body>
       </html>
