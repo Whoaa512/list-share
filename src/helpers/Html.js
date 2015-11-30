a733648272706d536e47a7360cf584229fecca69
C.J. Winslow <whoaa512@gmail.com>
2015-11-30 00:05:42 -0800

    fix ga

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -22,14 +22,13 @@ export default class Html extends Component {
   render () {
     const {assets, component, store} = this.props
     const content = component ? ReactDOM.renderToString(component) : ''
-    const googleAnalyticsCode = `<script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    const googleAnalyticsCode = `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
       ga('create', 'UA-70843519-1', 'auto');
-      ga('send', 'pageview');</script>`
+      ga('send', 'pageview');`
 
     return (
       <html lang='en-us'>
