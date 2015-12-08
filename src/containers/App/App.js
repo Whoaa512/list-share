b88ae66c1a4441a03c327bfd1e0a1dd6dd74eeed
C.J. Winslow <whoaa512@gmail.com>
2015-12-07 18:20:29 -0800

    always show the All Lists link if logged in

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -117,7 +117,7 @@ export default class App extends Component {
           <Navbar.Collapse>
             <Nav>
               {user &&
-              <LinkContainer className='visible-xs-block' to='/'>
+              <LinkContainer to='/'>
                 <NavItem eventKey={0}>All Lists</NavItem>
               </LinkContainer>}
               {user &&
