17f9a12fc81dde62fe063f7f4bba555e913ebc58
C.J. Winslow <whoaa512@gmail.com>
2016-01-03 12:43:52 -0800

    allow data urls for images

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -61,9 +61,27 @@ export function match (field) {
   }
 }
 
+// Detecting data URLs
+// data URI - MDN https://developer.mozilla.org/en-US/docs/data_URIs
+// The "data" URL scheme: http://tools.ietf.org/html/rfc2397
+// Valid URL Characters: http://tools.ietf.org/html/rfc2396#section2
+// Original Source: https://gist.github.com/bgrins/6194623
+export function validDataURL (value) {
+  if (!isEmpty(value) && !validDataURL.regex.test(value)) {
+    return 'Invalid Data Url'
+  }
+}
+validDataURL.regex = /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i
+
 export function validUrl (value) {
   if (!isEmpty(value) && !validUrlRegex.test(value)) {
     return 'Invalid url'
+  }
+}
+
+export function validImage (value) {
+  if (!validDataURL(value) || !validUrl(value)) {
+    return 'Invalid image'
   }
 }
 
