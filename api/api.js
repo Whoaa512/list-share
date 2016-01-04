29b2285b37ca9826d9cd94f1308c88bceaf6ab27
C.J. Winslow <whoaa512@gmail.com>
2016-01-03 16:04:28 -0800

    increase max session timeout to 1 month

--- api/api.js
+++ api/api.js
@@ -41,7 +41,7 @@ app.use(session({
     logFn: logger.debug
   }),
   unset: 'destroy',
-  cookie: { maxAge: 3600000 }
+  cookie: { maxAge: 2592000000 }
 }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
