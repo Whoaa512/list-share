58beb7031aa4f038b4071967bec5b627479cb52c
C.J. Winslow <whoaa512@gmail.com>
2016-01-04 23:07:11 -0800

    add api routes to update and load list meta data for user

---
+++ api/actions/listMeta/load.js
@@ -0,0 +1,18 @@
+import ApiError from 'utils/ApiError'
+import get from 'lodash.get'
+import { getUser } from 'actions/users/load'
+
+export default function load (req) {
+  return new Promise((resolve, reject) => {
+    const userId = get(req, 'session.user.id', false)
+    if (!userId) {
+      return reject(new ApiError('Missing session to get user id'))
+    }
+    const user = getUser(undefined, userId)
+    if (user == null) {
+      reject(new ApiError(`No user with that id: ${userId}`))
+    }
+    const listMeta = get(user, 'listMeta', {})
+    resolve(listMeta)
+  })
+}
