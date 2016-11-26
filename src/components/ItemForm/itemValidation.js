9e39c4ba3d018d6acd0028eda7670610d3c948fe
C.J. Winslow <whoaa512@gmail.com>
2016-11-26 12:11:29 -0800

    Remove image url validation

--- src/components/ItemForm/itemValidation.js
+++ src/components/ItemForm/itemValidation.js
@@ -2,13 +2,11 @@ import memoize from 'lru-memoize'
 import {
   createValidator,
   required,
-  validImage,
   validUrl
 } from 'utils/validation'
 
 const itemValidation = createValidator({
   link: validUrl,
-  imageUrl: validImage,
   title: required
 })
 export default memoize(10)(itemValidation)
